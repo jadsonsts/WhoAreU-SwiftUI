057fecb4c1d3527554cd5ab43d7e3c9058438c3d
jadsonsts <jadson.s.carneiro@gmail.com>
2022-12-20 22:36:12 +1300

    first commit

---
+++ WhoAreU/WhoAreUApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoAreUApp.swift
+//  WhoAreU
+//
+//  Created by Jadson on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WhoAreUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
